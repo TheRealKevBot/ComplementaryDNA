19a2191081696567d28c37c8939a43f745d2c7c2
TheRealKevBot <Kglas001@gmail.com>
2021-06-10 13:36:23 -0600

    First

---
+++ index.js
@@ -0,0 +1,16 @@
+//  In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+//  You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. 
+//  DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// DNAStrand ("ATTGC") returns "TAACG"
+// DNAStrand ("GTAT")  returns "CATA" 
+
+
+
+function DNAStrand(dna){
+    //your code here
+} 
+
+console.log(DNAStrand("AAAA"),"TTTT","String AAAA is");
+console.log(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
+console.log(DNAStrand("GTAT"),"CATA","String GTAT is");
